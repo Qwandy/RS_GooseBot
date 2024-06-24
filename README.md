51200bd03b9b14ccf08034e0c9d7cd71edc37c90
Qwandy <sarunas.slepetis@hotmail.com>
2024-06-24 12:39:47 +0100

    Updates readme with some documentation for the python and discord commands which can be used.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# RS_GooseBot
-A repository for hosting RS Goose Games
+# RSGooseBot
+A discord bot to aid with running clan goose game events
+
+## Plan for the project
+2. Create a list of optional inputs for tiles of a goose game (limit of 100 inputs?)
+    1. create command show_tiles which shows all of the tiles
+    2. create command update_tile in format /update_tile (number, role_name OR role_id)
+        1. using command should send a message announcing what tile the player has landed on
+4. Announce what tile players have landed on
+5. Look into hosting the bot
+6. Make some fun commands
+
+## What needs to be done for the event (outside of bot scope)
+1. Finish planning out the tasks
+2. Create a graphic of all the various tasks
+3. Advertise the event and poll various things
+
+## Documentation
+
+### Python commands
+- create_team_data(tblname): creates the table inside the tblname file (such as team_data.db)
+- insert_team_data(tblname, user_id, username, role_id, rolename): inserts all of the data into a database for one user.
+
+### Discord commands
+- /text hello: greets the user
+- /text flax: uses the user's name for a joke and embeds a gif in the text channel
+- /create_game: reads in data from a text file to generate an embed of various tasks for the goose game.
---
+++ main.py
@@ -0,0 +1,157 @@
+# imports
+import discord
+import dotenv
+import os
+from discord import channel
+from typing import List, Optional, Dict
+from dataclasses import dataclass
+import re
+import random
+import math
+import pandas as pd
+import sqlite3
+
+
+def create_task_data(txtfile):
+        with open(txtfile, 'r') as f:
+                lines = f.readlines()
+        task_numbers = [line.split(".")[0] for line in lines]
+        tasks = [line.split(".")[1].replace("\n", "") for line in lines]
+        
+        df = pd.DataFrame(columns=["Task Number", "Task"])
+        df["Task Number"] = task_numbers
+        pd.to_numeric(df["Task Number"])
+        df["Task"] = tasks
+        df.set_index("Task Number", drop = True, inplace=True)
+        return df 
+
+def create_team_data(tblfile):
+    
+    # SQL code
+    sql_statements = ["""CREATE TABLE IF NOT EXISTS team_data ( \
+            user_id INTEGER PRIMARY key, \
+                name text NOT NULL, \
+                role_id INTEGER, \
+                role_name text NOT NULL, \
+                board_position); """]
+    
+    try: 
+        with sqlite3.connect(tblfile) as conn:
+            cursor = conn.cursor()
+            for statement in sql_statements:
+                cursor.execute(statement)
+            conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+def insert_team_data(tblname, user_id, username, role_id, rolename):
+    
+    # testing inserting and viewing data
+    con = sqlite3.connect(tblname)
+    cur = con.cursor()
+    cur.execute(f"INSERT INTO team_data (user_id, name, role_id, role_name, board_position) \
+    VALUES ('{user_id}', '{username}', '{role_id}', '{rolename}', '0');")
+
+    for row in cur.execute("SELECT user_id, name, role_id, role_name, board_position FROM team_data"):
+        print(row)
+
+# initialises the bot and connects it to discord
+bot = discord.Bot()
+
+class FunCommands():
+
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user} is ready and online")
+
+    # a subgroup of commands for creating text
+    text = bot.create_group("text", "Greet people")
+
+    @text.command() # says hello to the user
+    async def hello(ctx):
+        await ctx.respond(f"Hello, {ctx.author}!")
+
+    @text.command() # a meme command that says user's flax be spinning and shows a gif
+    async def flax(self):
+        await discord.TextChannel.send(self, content = f"**{self.author}'s flax be spinnin...**", file= discord.File("memes/osrs-flax.gif"))
+
+class RSGooseBot():
+    
+    game = bot.create_group("game", "run goose game")
+
+    @game.command()
+    async def create_game(ctx):
+        txtfile = 'tasks.txt'
+        with open(txtfile, 'r') as f:
+            lines = f.readlines()
+        clean_lines = [line.replace("\n", "") for line in lines]
+        print(clean_lines)
+
+        if len(clean_lines) <= 25:
+            embed = discord.Embed(
+                title="Goose game tasks",
+                description = "Tasks",
+                color = discord.Color.blurple()
+            )
+
+            for i in range(len(clean_lines)):
+                embed.add_field(name=clean_lines[i], value = "Task", inline = False)
+            
+            embed.set_footer(text="Footer")
+            embed.set_author(name="McQwandy")
+
+            await ctx.respond("Hello! You have successfully created an instance of a goose game!.", embed=embed)
+        
+        elif len(clean_lines) > 25:
+            embed_nr = math.ceil(len(clean_lines) / 25)
+            ctr = 0
+            for i in range(embed_nr):
+                embed = discord.Embed(
+                title="Goose game tasks",
+                description = "Tasks",
+                color = discord.Color.blurple()
+                )
+                if ctr < 25:
+                    for k in range(len(clean_lines[0:25])):
+                        embed.add_field(name=clean_lines[ctr], value = "Task", inline = False)
+                        ctr += 1
+                        print(ctr)
+                elif ctr >= 25:
+                    for y in range(len(clean_lines[26:])+1):
+                        embed.add_field(name=clean_lines[ctr], value = "Task", inline = False)
+                        ctr += 1
+                        print(ctr)
+                
+                    
+                embed.set_footer(text="Footer")
+                embed.set_author(name="McQwandy")
+
+                await ctx.respond("Hello! You have successfully created an instance of a goose game!.", embed=embed)
+
+if __name__ == "__main__":
+    
+    dotenv.load_dotenv() # loads dotenv data into code
+    token = str(os.getenv("DISCORD_TOKEN")) # gets token as private variable
+
+    # create pandas dataframe of task data
+    df_tasks = create_task_data('tasks.txt')
+
+    create_team_data("team_data.db")
+
+    # testing inserting and viewing data
+    """con = sqlite3.connect("team_data.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO team_data (user_id, name, role_id, role_name, board_position) \
+    VALUES ('1234', 'qwandy', '1234', 'The_Mr', '0'), \
+                ('2345', 'lyra', '2345', 'The_Mrs', '0'); ")"""
+    
+    con = sqlite3.connect("team_data.db")
+    cur = con.cursor()
+
+    insert_team_data("team_data.db", "1234", "qwandy", "2345", "gigachad")
+    insert_team_data("team_data.db", "2345", "lyra", "2345", "gigachadess")
+
+    for row in cur.execute("SELECT user_id, name, role_id, role_name, board_position FROM team_data"):
+        print(row)
+
+    bot.run(token)
